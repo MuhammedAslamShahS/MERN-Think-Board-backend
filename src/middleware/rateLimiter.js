a72e7c6dbaf83c9584b0dcb9680d36c8d49cabd1
muhammedaslamshahs <muhammedaslamshahsofficieal@gmail.com.com>
2026-03-14 12:50:06 +0530

    5th commit : {solve rate limit problem multiple times create form, updated server.js each implimentation order, updated rateLimiter.js  to add dynamic identifier insted of static }

--- src/middleware/rateLimiter.js
+++ src/middleware/rateLimiter.js
@@ -2,21 +2,23 @@ import ratelimit from "../config/upstash.js";
 
 const rateLimit = async (req, res, next) => {
     try {
-        const { success } = await ratelimit.limit("my-rate-limit");
+        // ✅ UPDATED: use user IP instead of static key
+        const identifier = req.ip;
+
+        const { success } = await ratelimit.limit(identifier);
 
         if (!success) {
             return res.status(429).json({
-                 message: "Too many requests, please try again later." 
+                message: "Too many requests, please try again later.",
             });
         }
         next();
     } catch (error) {
-      console.log("Rate Limit error:", error);
-      next(error);
-      // res.status(500).json({ message: "Internal Server Error" });
-      // next(error);
-      // res.status(500).json({ message: "Internal Server Error" });
-      
+        console.log("Rate Limit error:", error);
+        next(error);
+        // res.status(500).json({ message: "Internal Server Error" });
+        // next(error);
+        // res.status(500).json({ message: "Internal Server Error" });
     }
 };
 
--- src/server.js
+++ src/server.js
@@ -7,21 +7,24 @@ import { connectDB } from "./config/db.js";
 import rateLimit from "./middleware/rateLimiter.js";
 
 dotenv.config();
+connectDB();
 
 console.log(process.env.MONGODB_URI);
+
 const app = express();
 
-// Middleware to parse JSON bodies
-app.use(express.json());
-app.use(rateLimit); // Apply rate limiting middleware to all routes
 app.use(
     cors({
         origin: "http://localhost:5173",
     }),
 );
-connectDB();
+// Middleware to parse JSON bodies
+app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+app.use(rateLimit); // Apply rate limiting middleware to all routes
+
+
+const PORT = process.env.PORT || 5001;
 
 app.use("/api/notes", notesRoutes);
 
